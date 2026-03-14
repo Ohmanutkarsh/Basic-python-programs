3b6b382e8890ef9b6b98b3ac3eece8cc4d34602a
Utkarsh Tapise <148682088+Ohmanutkarsh@users.noreply.github.com>
2026-03-14 22:44:34 +0530

    new guess the number game

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,17 +1,35 @@
-import random,sys
-RanNum = random.randint(1,20)
-print('I am thinking of a number between 1 to 20')
-
-for GuessTook in range(5) :
-    print('Take a guess')
-    guess = int(input())
-    if guess < RanNum:
-        print('Your guess is a too low')
-    elif guess > RanNum :
-        print('Your guess is a too high')
+import random,sys,time
+while True:
+    time.sleep(0.7)
+    print('to play enter (p) to quit enter (q) ')
+    replay = input()
+    time.sleep(0.7)
+    if replay == "q":
+        sys.exit()
+    elif replay =="p":
+        
+        RanNum = random.randint(1,20)
+        print('I am thinking of a number between 1 to 20')
+        time.sleep(0.7)
+        for GuessTook in range(5) :
+            time.sleep(0.7)
+            print('Take a guess')
+            guess = int(input())
+            if guess < RanNum - 5:
+                print('Your guess is too low')
+            elif RanNum - 5 <= guess < RanNum:
+                print('Your guess is a little low')
+            elif RanNum + 5 >= guess > RanNum:
+                print('Your guess is a little high')
+            elif guess > RanNum :
+                print('Your guess is a too high')
+            else :
+                break
+        if guess == RanNum:
+            time.sleep(0.7)
+            print('Good job, You guessed my number in ' + str(GuessTook+1) + ' guesses')
+        else :
+            time.sleep(0.7)
+            print('Nope i was thinking of ' + str(RanNum))
     else :
-        break
-if guess == RanNum:
-    print('Good job, You guessed my number in ' + str(GuessTook) + ' guesses')
-else :
-    print('Nope i was thinking of ' + str(RanNum))
+        print('choose apropriate option')
