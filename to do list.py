93990dd4487f418cbf804f7a58c9b7ea54bc5c88
Utkarsh Tapise <148682088+Ohmanutkarsh@users.noreply.github.com>
2026-03-21 21:44:08 +0530

    added delete and exit option

--- to do list.py
+++ to do list.py
@@ -1,35 +1,67 @@
 import time,sys
 
 while True:
-    choice = input('write / read / add \n')
+    choice = input('1 write\n2 read\n3 add\n4 delete\n5 exit\n')
     if choice == 'write':
-        f = open("ToDo_List.txt" , "w")
-        while True:
-            event = input("enter do's / write done if you are done \n")
-            if event != 'done':
-                f.write(event + "\n")
-            else :
-                break
-        f.close()
+        with open("ToDo_List.txt" , "w") as f :
+            while True:
+                event = input("enter do's / write done if you are done \n")
+                if event != 'done':
+                    f.writelines(event + "\n")
+                else :
+                    break
 
     elif choice == 'read':
-        f = open("ToDo_List.txt" , "r")
-        i = 1
-        for line in f:
-            print(str(i) + ' ' + line)
-            i += 1
-
-        f.close()
+        with open("ToDo_List.txt" , "r") as f :
+            lines = f.readlines()
+            if not lines :
+                print('no tasks awailable')
+            else :
+                for i, line in enumerate(lines):
+                    print((i + 1), line.strip())
 
     elif choice == 'add':
-        f = open("ToDo_List.txt" , "a")
-        while True:
-            event = input("enter do's / write done if you are done \n")
-            if event != 'done':
-                f.write(event + '\n')
+        with open("ToDo_List.txt" , "a") as f :
+            while True:
+                event = input("enter do's / write done if you are done \n")
+                if event != 'done':
+                    f.writelines(event + '\n')
+                else :
+                    break
+
+    elif choice == 'delete':
+        with open("ToDo_List.txt" , "r") as f :
+            lines = f.readlines()
+            if not lines :
+                print('no tasks available')
+                continue
             else :
+                for i, line in enumerate(lines):
+                    print((i + 1), line.strip())
+
+        while True:
+            index = input('which index of task you want to delete / write 0 once you are done \n')
+            try :
+                ind = int(index)
+            except ValueError :
+                print('enter valid index')
+                continue
+
+            if ind > len(lines) or ind < 0:
+                print('enter valid index')
+                continue
+            elif ind == 0 :
                 break
-        f.close()
+            else:
+                del lines[ind - 1]
+                with open("ToDo_List.txt" , "w") as f :
+                    f.writelines(lines)
+
+                print('task number ' + str(ind) +' is deleted')
+
+
+    elif choice == 'exit':
+       sys.exit()
 
     else:
         print("invalid option")
